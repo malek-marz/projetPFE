7133b993dcb0e17c5e1f1ecde1e0bb11a62f9c77
malek_marz <malekmarzouki03@gmail.com>
2025-02-25 16:33:38 +0100

    bonjour

--- app/src/main/java/com/example/testapp/features/login/LoginViewModel.kt
+++ app/src/main/java/com/example/testapp/features/login/LoginViewModel.kt
@@ -1,43 +1,59 @@
 package com.example.testapp.features.login
 
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.testapp.features.login.model.LoginState
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
     private val _state = MutableStateFlow(LoginState())
     val state: StateFlow<LoginState> = _state
 
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     fun updateEmail(email: String) {
-        _state.value = _state.value.copy(email = email)
+        _state.update { it.copy(email = email) }
     }
 
     fun updatePassword(password: String) {
-        _state.value = _state.value.copy(password = password)
+        _state.update { it.copy(password = password) }
     }
 
-    fun validateCredentials(): Boolean {
+    fun togglePasswordVisibility() {
+        _state.update { it.copy(passwordVisible = !it.passwordVisible) }
+    }
+
+    private fun validateCredentials(): Boolean {
         return if (_state.value.email.isEmpty() || _state.value.password.isEmpty()) {
-            _state.value = _state.value.copy(errorMessage = "Veuillez remplir tous les champs")
+            _state.update { it.copy(errorMessage = "Veuillez remplir tous les champs") }
             false
         } else {
-            _state.value = _state.value.copy(errorMessage = "")
+            _state.update { it.copy(errorMessage = "") }
             true
         }
     }
-    fun login():Boolean{
-        return if(validateCredentials()){
-            //firebase
-            true
-        }
-        else{
 
-            false
-        }
+    fun login(onLoginSuccess: () -> Unit) {
+        if (!validateCredentials()) return
+
+        _state.update { it.copy(isLoading = true) }
+
+        val email = _state.value.email
+        val password = _state.value.password
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _state.update { it.copy(errorMessage = "Connexion réussie", isLoading = false) }
+                    onLoginSuccess()  // Redirige après connexion réussie
+                } else {
+                    _state.update { it.copy(errorMessage = "Échec : ${task.exception?.message}", isLoading = false) }
+                }
+                
+            }
     }
-
 }
